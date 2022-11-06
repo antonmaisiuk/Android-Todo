c95fab21d0df7874f3c5115d3a6ce77d5a483340
Anton <superantoxa12@gmail.com>
2022-11-06 11:28:18 +0100

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+o
+?app/src/main/java/com/example/android_todoapp/TaskFragment.java,b\8\b8d782a042a59def0cae5a1cafb69d6f67cbe428
+g
+7app/src/main/java/com/example/android_todoapp/Task.java,c\c\cc732e00db16a9cc164b64e61e9cd403cfa00ab0
+o
+?app/src/main/java/com/example/android_todoapp/MainActivity.java,6\d\6dacb15048a21bf1787aa1fa36ffd621ef9b22bd
---
+++ app/src/main/java/com/example/android_todoapp/Task.java
@@ -0,0 +1,32 @@
+package com.example.android_todoapp;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Task {
+    private UUID id;
+    private String name;
+    private Date date;
+    private boolean done;
+
+    public Task(){
+        id = UUID.randomUUID();
+        date = new Date();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean isChecked) {
+        this.done = isChecked;
+    }
+}
